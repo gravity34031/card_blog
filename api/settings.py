755e37323351d71768f5b6bec0c7d8cde3b7f0d4
gravity34031 <gravity2507@gmail.com>
2023-01-05 23:03:26 +0500

    10

--- api/settings.py
+++ api/settings.py
@@ -208,12 +208,19 @@ PHOTOLOGUE_IMAGE_FIELD_MAX_LENGTH = 128
 
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+""" EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'gravity2507@gmail.com'
-EMAIL_HOST_PASSWORD = 'ohkmvjkuvujpvukw'
+EMAIL_HOST_PASSWORD = 'ohkmvjkuvujpvukw' """
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 
 
 
